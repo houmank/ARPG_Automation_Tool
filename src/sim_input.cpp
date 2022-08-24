ee4a0d0fa1f43f8742532078e1db92406ffc7521
houmank <43026293+houmank@users.noreply.github.com>
2022-08-24 16:19:20 -0400

    adjusted sleep time to 100ms between key presses

--- src/sim_input.cpp
+++ src/sim_input.cpp
@@ -45,6 +45,9 @@ namespace Input
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
         SendInput(1, &input, sizeof(input));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
     // if press is true, it will only send KEY_DOWN flag, else it will release
     void InputHandler::keyReleaseOrHold(DWORD virtualKey, bool press)
     {
